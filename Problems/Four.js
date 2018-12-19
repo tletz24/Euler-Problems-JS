fbdd5baaeebfd962210eae533921a76ee85e125f
Trenton Letz <trentonletz@trentons-mbp.lan>
2018-12-19 11:28:37 -0500

    [Utils] Palindrome
    - Added palindrome to the Utils library
    - Reformatted #4 to include function from palindrome.js

--- Problems/Four.js
+++ Problems/Four.js
@@ -1,19 +1,4 @@
-function checkPalindrome(number) {
-    let rem, temp, final = 0;
-
-    temp = number;
-    while(number > 0) {
-        rem = number % 10;
-        number = parseInt(number / 10);
-        final = final * 10 + rem;
-    }
-
-    if (final == temp) {
-        return true;
-    } else {
-        return false;
-    }
-}
+import checkNumberPalindrome() from '../Utils/palindrome';
 
 largest = null;
 
@@ -21,7 +6,7 @@ for (let p = 999; p > 99; p--) {
     for (let q = 999; q > 99; q--) {
         temp = p * q;
 
-        if(checkPalindrome(temp) && temp > largest) {
+        if(checkNumberPalindrome(temp) && temp > largest) {
             largest = temp;
         }
     }
--- Utils/palindrome.js
+++ Utils/palindrome.js
@@ -8,21 +8,21 @@
  *
  * @param {any} value any type to evaluate
  */
-export const isPalindrome = (value) => {
+const isPalindrome = (value) => {
     switch (typeof value) {
         case 'number':
         return checkNumberPalindrome(value);
         case 'string':
         return checkStringPalindrome(value);
     }
-}
+};
 
 /**
  * Function to determine if a given number is a Palindrome.
  *
  * @param {int} number integer to eval.
  */
-export const checkNumberPalindrome = (number) => {
+export checkNumberPalindrome(number) {
     let tmp = number, reverse = 0;
 
     while(tmp > 0) {
@@ -33,19 +33,21 @@ export const checkNumberPalindrome = (number) => {
     }
 
     return (reverse == number) ? true : false;
-}
+};
 
 /**
  * Function to determine if a given string is a Palindrome.
- * Credit: Sonya Moisset
+ * @author: Sonya Moisset
  * @see https://medium.freecodecamp.org/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7
  *
  * @param {string} str string to eval.
  */
-export const checkStringPalindrome(str) {
+const checkStringPalindrome = (str) => {
     const re = /[\W_]/g;
     const lowRegStr = str.toLowerCase().replace(re, '');
     const reverseStr = lowRegStr.split('').reverse().join('');
 
     return reverseStr === lowRegStr;
 }
+
+export default isPalindrome, checkNumberPalindrome;
