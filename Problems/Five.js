b9d92717b0d9204a9609b50f7d7974416703aa2c
Trenton Letz <trentonsletz@icloud.com>
2019-02-09 15:02:56 -0500

    Added a comparison tracker to Probelm 5

--- Problems/Five.js
+++ Problems/Five.js
@@ -13,16 +13,16 @@
 console.time('Problem 5');
 
 let i = 20;
+let comparisons = 0;
 
 while (i % 20 !== 0 || i % 19 !== 0 || i % 18 !== 0
     || i % 17 !== 0 || i % 16 !== 0 || i % 15 !== 0
-    || i % 13 !== 0 || i % 12 !== 0 || i % 11 !== 0
-    || i % 10 !== 0 || i % 9 !== 0 || i % 8 !== 0
-    || i % 7 !== 0 || i % 6 !== 0 || i % 5 !== 0
-    || i % 4 !== 0 || i % 3 !== 0 || i % 2 !== 0) {
-  i += 20;
+    || i % 13 !== 0 || i % 12 !== 0 || i % 11 !== 0) {
+  comparisons += 1;
+  i += 4;
 }
 
 console.timeEnd('Problem 5');
 
 console.log(`Smallest number is ${i}`);
+console.log(`# of comparisons: ${comparisons}`);
